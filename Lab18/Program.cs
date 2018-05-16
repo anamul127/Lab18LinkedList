ab7b117e51c4135b6cdc0a19be71c1972dca4d8b
Anamul Islam <anamul.islam127@gmail.com>
2018-05-16 08:50:49 -0400

    first commit

---
+++ Lab18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddAtStart("Adam");
+            list.AddAtStart("Cat");
+            list.AddAtStart("Mohammed");
+            list.AddAtStart("Shaun");
+            list.AddToEnd("Shaun");
+            list.AddToEnd("Jaide");
+            list.AddAtStart("Shaun");
+
+            //list.PrintAllNodes
+
+            string getRid = "Shaun";
+            list.RemoveAll(getRid);
+
+
+            
+        }
+    }
+}
